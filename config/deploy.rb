b0e4f9eba3eb122b7050503f3f98cd487c8c76a2
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2016-03-11 17:26:00 +0800

    Set up project

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+set :application, "code-red-api"
+set :repo_url, "git@github.com:mcdave029/code-red-api.git"
+
+# Project-specific overrides go here.
+# For list of variables that can be customized, see:
+# https://github.com/mattbrictson/capistrano-mb/blob/master/lib/capistrano/tasks/defaults.rake
+
+fetch(:mb_recipes) << "sidekiq"
+fetch(:mb_aptitude_packages).merge!(
+  "redis-server@ppa:rwky/redis" => :redis
+)
+
+set :mb_dotenv_keys, %w(
+  rails_secret_key_base
+  postmark_api_key
+  sidekiq_web_username
+  sidekiq_web_password
+)
