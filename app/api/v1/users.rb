bfe8db4dfb4654442d4db216208f85bc12359891
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2016-03-15 14:44:49 +0800

    add medical records

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -16,6 +16,8 @@ module V1
 			desc "Create User Respondee"
 
 			params do
+				requires :blood_type, type: String
+				requires :medical_conditions, type: String
 				requires :user, type: Hash do
 					requires :name, type: String
 					requires :user_type, type: String
@@ -35,6 +37,11 @@ module V1
 				user = User.new(user_params);
 				user.avatar = ActionDispatch::Http::UploadedFile.new(attachment)
 				user.save
+				@medical_record = user.build_medical_record({
+						blood_type: params[:blood_type],
+						medical_conditions: params[:medical_conditions]
+					})
+				@medical_record.save
 			end
 
 		end
