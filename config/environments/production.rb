5703cd16d546a265dd61ec54ab905cb5070363e5
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2016-03-22 18:48:28 +0800

    remove postmark test deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,9 +71,9 @@ Rails.application.configure do
 
   # Production email config
   config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = {
-    :api_token => ENV.fetch("POSTMARK_API_KEY")
-  }
+  # config.action_mailer.postmark_settings = {
+  #   :api_token => ENV.fetch("POSTMARK_API_KEY")
+  # }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {
     host: "example.com",
