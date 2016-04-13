54dd70b313a0dc6a8809c3fef0b90313fc050191
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2016-04-14 01:10:17 +0800

    change report attributes and add reports respondee and responder

---
+++ db/migrate/20160413135846_create_interactors.rb
@@ -0,0 +1,10 @@
+class CreateInteractors < ActiveRecord::Migration
+  def change
+    create_table :interactors do |t|
+      t.references :user, index: true, foreign_key: true
+      t.references :report, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ test/models/interactor_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class InteractorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
