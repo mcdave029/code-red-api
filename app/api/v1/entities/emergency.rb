fb1453b692882f3f75f8aebd16e11ec08120b1fe
Mc Dave Valdecantos <mdvaldecantos@gmail.com>
2016-04-17 10:18:02 +0800

    add classification to registration contacts

--- app/api/v1/entities/emergency.rb
+++ app/api/v1/entities/emergency.rb
@@ -2,6 +2,7 @@ module V1
 	module Entities
 		module Emergency
 			class Contacts < Grape::Entity
+				expose :classification
 				expose :name, :documentation => {:type => "string", :desc => "Emergency contact name"}
 				expose :contact, :documentation => {:type => "string", :desc => "Emergency contact number"}
 			end
